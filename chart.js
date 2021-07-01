7dc1c5b1f3d40d6fb0476a05df7b13d4994ecf1b
Jeffery Anderson <jeff@andersoncreativedesign.com>
2021-07-01 01:28:05 -0500

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,151 +1,123 @@
-const sampleData = [{
-    date: "2021-01-15T10:00:00.000",
-    price: .5
-},
-{
-    date: "2021-01-16T10:00:00.000",
-    price: 1.5
-},
-{
-    date: "2021-01-17T10:00:00.000",
-    price: 1.4
-},
-{
-    date: "2021-01-18T10:00:00.000",
-    price: 1.3
-},
-{
-    date: "2021-01-19T10:00:00.000",
-    price: 1.2
-},
-{
-    date: "2021-01-20T10:00:00.000",
-    price: 2.5
-},
-{
-    date: "2021-01-21T10:00:00.000",
-    price: 2.0
-}];
+const colors = ['#DE6E51', '#E8A339', '#68B253'];
 
-const formatedData = d3.csvFormat(sampleData);
-formatedData && console.log('formatData=', formatedData);
-/* Line 1: "Feed", unit=Kilograms per hector (kg/ha)
-[
-    {
-        timestamp: "2021-01-15T10:00:00.000",
-        value: .5
-    },
-    {
-        timestamp: "2021-01-16T10:00:00.000",
-        value: 1.5
-    },
-    {
-        timestamp: "2021-01-17T10:00:00.000",
-        value: 1.4
-    },
-    {
-        timestamp: "2021-01-18T10:00:00.000",
-        value: 1.3
-    },
-    {
-        timestamp: "2021-01-19T10:00:00.000",
-        value: 1.2
-    },
-    {
-        timestamp: "2021-01-20T10:00:00.000",
-        value: 2.5
-    },
-    {
-        timestamp: "2021-01-21T10:00:00.000",
-        value: 2.0
-    }
-]Line 2: "Biomass", unit=Kilograms per hector (kg/ha)
-[
-    {
-        timestamp: "2021-01-15T10:00:00.000",
-        value: 10
-    },
-    {
-        timestamp: "2021-01-16T10:00:00.000",
-        value: 11
-    },
-    {
-        timestamp: "2021-01-17T10:00:00.000",
-        value: 12
-    },
-    {
-        timestamp: "2021-01-18T10:00:00.000",
-        value: 12
-    },
-    {
-        timestamp: "2021-01-19T10:00:00.000",
-        value: 13.5
-    },
-    {
-        timestamp: "2021-01-20T10:00:00.000",
-        value: 9
-    },
-    {
-        timestamp: "2021-01-21T10:00:00.000",
-        value: 8
-    }
-]Line 3: "Ammonia", unit=Kilograms per hector (kg/ha)
-[
-     {
-        timestamp: "2021-01-15T10:00:00.000",
-        value: 5.1
-    },
-    {
-        timestamp: "2021-01-16T10:00:00.000",
-        value: 7
-    },
-    {
-        timestamp: "2021-01-17T10:00:00.000",
-        value: 7
-    },
-    {
-        timestamp: "2021-01-18T10:00:00.000",
-        value: 6
-    },
-    {
-        timestamp: "2021-01-19T10:00:00.000",
-        value: 5
-    },
-    {
-        timestamp: "2021-01-20T10:00:00.000",
-        value: 4
-    },
-    {
-        timestamp: "2021-01-21T10:00:00.000",
-        value: 3.1
-    }
-]
 
- */
-console.log('formatted date =', new Date("2021-01-21T10:00:00.000"));
+const sampleData = [
+    {
+        "name": "Feed",
+        "unit": "Kilograms per hector (kg/ha)",
+        "values": [
+            {
+                "timestamp": "2021-01-15T10:00:00.000",
+                "value": 0.5
+            },
+            {
+                "timestamp": "2021-01-16T10:00:00.000",
+                "value": 1.5
+            },
+            {
+                "timestamp": "2021-01-17T10:00:00.000",
+                "value": 1.4
+            },
+            {
+                "timestamp": "2021-01-18T10:00:00.000",
+                "value": 1.3
+            },
+            {
+                "timestamp": "2021-01-19T10:00:00.000",
+                "value": 1.2
+            },
+            {
+                "timestamp": "2021-01-20T10:00:00.000",
+                "value": 2.5
+            },
+            {
+                "timestamp": "2021-01-21T10:00:00.000",
+                "value": 2.0
+            }
+        ]
+    },
+    {
+        "name": "Biomass",
+        "unit": "Kilograms per hector (kg/ha)",
+        "values": [
+            {
+                "timestamp": "2021-01-15T10:00:00.000",
+                "value": 10
+            },
+            {
+                "timestamp": "2021-01-16T10:00:00.000",
+                "value": 11
+            },
+            {
+                "timestamp": "2021-01-17T10:00:00.000",
+                "value": 12
+            },
+            {
+                "timestamp": "2021-01-18T10:00:00.000",
+                "value": 12
+            },
+            {
+                "timestamp": "2021-01-19T10:00:00.000",
+                "value": 13.5
+            },
+            {
+                "timestamp": "2021-01-20T10:00:00.000",
+                "value": 9
+            },
+            {
+                "timestamp": "2021-01-21T10:00:00.000",
+                "value": 8
+            }
+        ]
+    },
+    {
+        "name": "Ammonia",
+        "unit": "Kilograms per hector (kg/ha)",
+        "values": [
+            {
+                "timestamp": "2021-01-15T10:00:00.000",
+                "value": 5.1
+            },
+            {
+                "timestamp": "2021-01-16T10:00:00.000",
+                "value": 7
+            },
+            {
+                "timestamp": "2021-01-17T10:00:00.000",
+                "value": 7
+            },
+            {
+                "timestamp": "2021-01-18T10:00:00.000",
+                "value": 6
+            },
+            {
+                "timestamp": "2021-01-19T10:00:00.000",
+                "value": 5
+            },
+            {
+                "timestamp": "2021-01-20T10:00:00.000",
+                "value": 4
+            },
+            {
+                "timestamp": "2021-01-21T10:00:00.000",
+                "value": 3.1
+            }
+        ]
+    }
+];
+
+// set the dimensions and margins of the graph
 const margin = { top: 40, right: 80, bottom: 60, left: 50 },
     width = 960 - margin.left - margin.right,
     height = 280 - margin.top - margin.bottom;
 
 const parseDate = d3.timeParse("%m/%d/%Y");
-const formatDate = d3.timeFormat("%b %d");
+const formatDate = d3.timeFormat("%m %d, %Y");
 const formatMonth = d3.timeFormat("%b");
 const x = d3.scaleTime().range([0, width]);
 const y = d3.scaleLinear().range([height, 0]);
 
-const area = d3
-    .area()
-    .x((d) => { return x(d.date); })
-    .y0(height)
-    .y1((d) => { return y(d.price); })
-    .curve(d3.curveCardinal);
-
-const valueline = d3
-    .line()
-    .x((d) => { return x(d.date); })
-    .y((d) => { return y(d.price); })
-    .curve(d3.curveCardinal);
-
+// append the svg object to the body of the page
 const svg = d3
     .select("#root")
     .append("svg")
@@ -155,97 +127,89 @@ const svg = d3
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+/* x axis */
+
 svg
     .append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x).tickFormat(formatMonth));
+    .call(d3.axisBottom(x).ticks(3).tickFormat(formatDate));
+
+/* y axis */
 
 svg.append("g").attr("class", "y axis").call(d3.axisLeft(y));
 
+/* y axis lable */
+
 svg
     .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x", 0 - height / 2)
+    .attr("y", -20)
+    .attr("x", -20)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .text("USD");
+    .text("kg/ha");
 
-svg
-    .append("a")
-    .attr("xlink:href", (d) => {
-        return "https://www.moex.com/ru/index/rtsusdcur.aspx?tid=2552";
-    })
-    .attr("class", "subtitle")
-    .attr("target", "_blank")
-    .append("text")
-    .attr("x", 0)
-    .attr("y", height + 50)
-    .text("Source: Moscow Exchange");
+appendData(sampleData);
 
-appendData(2020);
-appendData(2019);
+function appendData(sampleData) {
 
-function appendData(year) {
-    d3.selectAll("path.area").remove();
-    d3.selectAll("path.line").remove();
-    d3.selectAll(".title").remove();
+    /* combine all values for domain */
+    const allValues = sampleData.map(d => d.values).flat();
 
-    filename = "https://raw.githubusercontent.com/jukuznets/datasets/main/usd-" + year + ".csv";
-    d3.csv(filename).then((data) => {
-        console.log('d3.csv=', data);
+    sampleData.forEach((line, index) => {
 
-        data = data.reverse();
-        console.log('d3.csv reversed=', data);
-        data.forEach((d) => {
-            // d.date = new Date(d.date);
-            //  console.log("d.date=", d.date);
-            d.date = parseDate(d.date);
-            // console.log("d.date==", d.date);
-            d.price = Number(d.price);
+        // line.values = line.values.reverse();
+
+        line.values.forEach((d) => {
+            d.timestamp = new Date(d.timestamp);
+            d.value = Number(d.value);
         });
 
         x.domain(
-            d3.extent(data, (d) => { return d.date; })
+            d3.extent(allValues, (d) => d.timestamp)
         );
         y.domain([
-            55,
-            d3.max(data, (d) => { return d.price; }),
+            0,
+            d3.max(allValues, (d) => d.value),
         ]);
 
+        /* animate axis */
         svg
             .select(".x.axis")
             .transition()
             .duration(750)
-            .call(d3.axisBottom(x).tickFormat(d3.timeFormat("%b")));
+            .call(d3.axisBottom(x).ticks(3).tickFormat(formatDate));
         svg
             .select(".y.axis")
             .transition()
             .duration(750)
             .call(d3.axisLeft(y));
 
-        /*   const areaPath = svg
-              .append("path")
-              .data([data])
-              .attr("class", "area")
-              .attr("d", area)
-              .attr("transform", "translate(0,300)")
-              .transition()
-              .duration(1000)
-              .attr("transform", "translate(0,0)"); */
+        /* The data lines */
 
-        const linePath = svg
-            .append("path")
-            .data([data])
-            .attr("class", "line")
-            .attr("d", valueline)
+        const linePath =
+            svg.append("path")
+                .data([line.values])
+                .attr("class", `line-${index}`)
+                .attr("d", d3
+                    .line()
+                    .x((d) => x(d.timestamp))
+                    .y((d) => y(d.value))
+                    .curve(d3.curveCardinal))
 
-        const dataPoint = svg.append("circle")
-            .data([data])
-            .attr("class", "y")
-            .style("fill", "none")
-            .attr("r", 4);
+        /* data point circles */
+
+        line.values.forEach(d => {
+            svg.selectAll()
+                .data([d])
+                .enter()
+                .append("circle")
+                .attr("fill", "white")
+                .attr("stroke", colors[index])
+                .attr("cx", (d) => x(d.timestamp))
+                .attr("cy", (d) => y(d.value))
+                .attr("r", 3)
+        })
 
         const pathLength = linePath.node().getTotalLength();
 
@@ -258,13 +222,23 @@ function appendData(year) {
             .attr("stroke-width", 0)
             .attr("stroke-dashoffset", 0);
 
+        /* dynamic legend text */
         svg
             .append("text")
             .attr("class", "title")
-            .attr("x", width / 2)
+            .attr("x", (width / 2) + 100 * (index - 1))
             .attr("y", 0 - margin.top / 2)
-            .attr("text-anchor", "middle")
-            .text("USD to RUB Exchange Rates, " + year);
+            .attr("text-anchor", "left")
+            .text(line.name);
+
+        /* dynamic legend dots */
+
+        svg.append("circle")
+            .attr("fill", colors[index])
+            .attr("stroke", "none")
+            .attr("cx", ((width / 2) + 100 * (index - 1)) - 8)
+            .attr("cy", -4 - margin.top / 2)
+            .attr("r", 4)
 
         const focus = svg
             .append("g")
@@ -300,45 +274,45 @@ function appendData(year) {
         focus.append("text").attr("class", "y4").attr("dx", 8).attr("dy", "1em");
 
         function mouseMove(event) {
-            const bisect = d3.bisector((d) => d.date).left,
-                x0 = x.invert(d3.pointer(event, this)[0]),
-                i = bisect(data, x0, 1),
-                d0 = data[i - 1],
-                d1 = data[i],
-                d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+            const bisect = d3.bisector((d) => d.timestamp).left;
+            const x0 = x.invert(d3.pointer(event, this)[0]);
+            const i = bisect(line.values, x0, 1);
+            const d0 = line.values[i - 1];
+            const d1 = line.values[i];
+            const d = x0 - d0.timestamp > d1.timestamp - x0 ? d1 : d0;
 
             focus
                 .select("circle.y")
-                .attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")");
+                .attr("transform", "translate(" + x(d.timestamp) + "," + y(d.value) + ")");
 
             focus
                 .select("text.y1")
-                .attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")")
-                .text(d.price);
+                .attr("transform", "translate(" + x(d.timestamp) + "," + y(d.value) + ")")
+                .text(`${line.name} ${d.value} ${line.unit}`);
 
-            focus
-                .select("text.y2")
-                .attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")")
-                .text(d.price);
+            /*    focus
+                   .select("text.y2")
+                   .attr("transform", "translate(" + x(d.timestamp) + "," + y(d.value) + ")")
+                   .text(d.value); */
 
             focus
                 .select("text.y3")
-                .attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")")
-                .text(formatDate(d.date));
+                .attr("transform", "translate(" + x(d.timestamp) + "," + y(d.value) + ")")
+                .text(formatDate(d.timestamp));
 
             focus
                 .select("text.y4")
-                .attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")")
-                .text(formatDate(d.date));
+                .attr("transform", "translate(" + x(d.timestamp) + "," + y(d.value) + ")")
+                .text(formatDate(d.timestamp));
 
             focus
                 .select(".x")
-                .attr("transform", "translate(" + x(d.date) + "," + y(d.price) + ")")
-                .attr("y2", height - y(d.price));
+                .attr("transform", "translate(" + x(d.timestamp) + "," + y(d.value) + ")")
+                .attr("y2", height - y(d.value));
 
             focus
                 .select(".y")
-                .attr("transform", "translate(" + width * -1 + "," + y(d.price) + ")")
+                .attr("transform", "translate(" + width * -1 + "," + y(d.value) + ")")
                 .attr("x2", width + width);
         }
 
@@ -355,5 +329,6 @@ function appendData(year) {
                 focus.style("display", "none");
             })
             .on("touchmove mousemove", mouseMove);
+
     });
 }
